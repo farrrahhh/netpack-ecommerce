054af920f1fb14347545510a9c11fa4fc9794d22
farrrahhh <18222096@std.stei.itb.ac.id>
2025-03-27 15:16:36 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white text-4xl font-bold">
+      Tailwind is working 🎉
+    </div>
+  );
+}
